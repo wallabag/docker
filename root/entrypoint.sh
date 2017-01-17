4de27027b98d6e5e1abd3913992f0e0c2d96ebc7
Jan Losinski <losinski@wh2.tu-dresden.de>
2017-01-17 06:47:09 +0100

    Add ability to run wannabag redis importer worker
    This adds the ability to run wallabag async import worker that
    interacts with a redis service.
    
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -4,5 +4,10 @@ if [ "$1" = "wallabag" ];then
     ansible-playbook -i /etc/ansible/hosts /etc/ansible/entrypoint.yml -c local
     exec s6-svscan /etc/s6/
 fi
+if [ "$1" = "import" ];then
+    ansible-playbook -i /etc/ansible/hosts /etc/ansible/entrypoint.yml -c local --skip-tags=firstrun
+    cd /var/www/wallabag/
+    exec su -c "bin/console wallabag:import:redis-worker -e=prod $2 -vv" -s /bin/sh nobody
+fi
 
 exec "$@"
